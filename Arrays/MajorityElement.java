b7065c7e32ae7f9431356aa7dafe6dbf77168b74
bansalbhavishaya <bhavishayabansal31@gmail.com>
2023-11-12 20:49:05 +0530

    12 nov

---
+++ Arrays/MajorityElement.java
@@ -0,0 +1,30 @@
+public class MajorityElement {
+    static int majorityElement(int a[], int size)
+    {
+        int el = 0;
+        int cnt = 0;
+        for(int i=0;i<size;i++){
+            if(cnt ==0){
+                cnt = 1;
+                el = a[i];
+            }
+            else if(el == a[i]){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
+        }
+        int cnt2 = 0;
+        for(int i=0;i<size;i++){
+            if(el == a[i]){
+                cnt2++;
+            }
+        }
+        if(cnt2>(size)/2){
+            return el;
+        }
+        return -1;
+    }
+}
+
